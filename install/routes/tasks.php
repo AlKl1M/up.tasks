79dab63226159f2833dbb8e87ed105802b48ebb4
AlKl1M <al.klimkovetskiy@gmail.com>
2023-03-25 17:44:11 +0300

    Добавление задач.

--- install/routes/tasks.php
+++ install/routes/tasks.php
@@ -6,4 +6,5 @@ use Bitrix\Main\Routing\RoutingConfigurator;
 return function (RoutingConfigurator $routes) {
 	$routes->get('/', new PublicPageController('/local/modules/up.tasks/views/tasks-list.php'));
 	$routes->get('/delete/{id}/', new PublicPageController('/local/modules/up.tasks/views/tasks-delete.php'))->where('id', '[0-9]+');
+	$routes->post('/', new PublicPageController('/local/modules/up.tasks/views/tasks-create.php'));
 };
--- lib/Task.php
+++ lib/Task.php
@@ -15,4 +15,15 @@ class Task
 	{
 		\Up\Tasks\TaskTable::delete($id);
 	}
+
+	public static function createTask($arg)
+	{
+		return \Up\Tasks\TaskTable::createObject()
+			->setTitle($arg['title'])
+			->setDescription($arg['description'] ?: '')
+			->setDateCreation(new \Bitrix\Main\Type\DateTime())
+			->setDateDeadline(((new \Bitrix\Main\Type\DateTime($arg['deadline'], 'Y-m-d'))) ?: '')
+			->setPriority($arg['priority'])
+			->save();
+	}
 }
